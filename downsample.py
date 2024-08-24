37eb8c1e4a59cb9bdaf8060fe9b8e0e5d6896457
Nicolas Boganski <nicolas.boganski@epita.fr>
2024-08-24 22:41:30 +0200

    Fix downsampling

--- downsample.py
+++ downsample.py
@@ -12,12 +12,11 @@ from tqdm import tqdm
 import zipfile
 
 
-def yield_wavs(zip_file: zipfile.ZipFile):
-    zip_path = zipfile.Path(zip_file, at="VCTK-Corpus/wav48/")
+def yield_wavs(zip_path: zipfile.Path):
     res = []
     for f in filter(
             lambda x: all(
-                substring not in x.name for substring in ["s5", "p280", "p315"]
+                substring not in x.name for substring in ["s5", "p280", "p315", "log.txt"]
             ),
             zip_path.iterdir(),
     ):
@@ -30,7 +29,7 @@ def process(wav_path: zipfile.Path):
     # speaker 's5', 'p280', 'p315' are excluded,
     speaker = wav_name[:4]
     # wav_path = os.path.join(args.in_dir, speaker, wav_name)
-    wav, sr = librosa.load(wav_path.open(mode='rb'))
+    wav, sr = librosa.load(wav_path.open(mode="rb"))
     wav, _ = librosa.effects.trim(wav, top_db=20)
     peak = np.abs(wav).max()
     if peak > 1.0:
@@ -38,28 +37,37 @@ def process(wav_path: zipfile.Path):
     wav = librosa.resample(wav, orig_sr=sr, target_sr=args.sr)
     save_name = wav_name.replace("_mic2.flac", ".wav")
     save_path = os.path.join(args.out_dir, speaker, save_name)
-    wavfile.write(
-        save_path,
-        args.sr,
-        (wav * np.iinfo(np.int16).max).astype(np.int16)
-    )
+    wavfile.write(save_path, args.sr, (wav * np.iinfo(np.int16).max).astype(np.int16))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--sr", type=int, default=16000, help="sampling rate")
-    parser.add_argument("--in_zip", type=Path, default="VCTK-Corpus.zip", help="path to source archive")
-    parser.add_argument('--num_workers', type=int, default=6)
-    parser.add_argument("--out_dir", type=Path, default="./dataset/vctk-16k", help="path to target dir")
+    parser.add_argument(
+        "--in_zip",
+        type=Path,
+        default="VCTK-Corpus-0.92.zip",
+        help="path to source archive",
+    )
+    parser.add_argument("--num_workers", type=int, default=8)
+    parser.add_argument(
+        "--out_dir", type=Path, default="./dataset/vctk-16k", help="path to target dir"
+    )
     args = parser.parse_args()
 
     args.out_dir.mkdir(exist_ok=True, parents=True)
 
     with zipfile.ZipFile(args.in_zip) as z:
-        for e in zipfile.Path(z, at="VCTK-Corpus/wav48/").iterdir():
+        path = zipfile.Path(z, at="wav48_silence_trimmed/")
+        for e in filter(
+            lambda x: all(
+                substring not in x.name
+                for substring in ["s5", "p280", "p315", "log.txt"]
+            ),
+            path.iterdir(),
+        ):
             (args.out_dir / e.name).mkdir(exist_ok=True)
 
-        files = yield_wavs(z)
-        process(files[0])
-        # with ThreadPoolExecutor(args.num_workers) as executor:
-        #     executor.map(process, files)
+        files = yield_wavs(path)
+        with ThreadPoolExecutor(args.num_workers) as executor:
+            executor.map(process, files)
