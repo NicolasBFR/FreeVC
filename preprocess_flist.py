e1529ef7c5af8e93dc6e11c073052313fd345c56
Nicolas Boganski <nicolas.boganski@epita.fr>
2024-08-25 02:36:16 +0200

    Fix preprocess_flist.py

--- preprocess_flist.py
+++ preprocess_flist.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+from pathlib import Path
+
 from tqdm import tqdm
 from random import shuffle
 
@@ -9,43 +11,43 @@ if __name__ == "__main__":
     parser.add_argument("--train_list", type=str, default="./filelists/train.txt", help="path to train list")
     parser.add_argument("--val_list", type=str, default="./filelists/val.txt", help="path to val list")
     parser.add_argument("--test_list", type=str, default="./filelists/test.txt", help="path to test list")
-    parser.add_argument("--source_dir", type=str, default="./dataset/vctk-16k", help="path to source dir")
+    parser.add_argument("--source_dir", type=Path, default="./dataset/vctk-16k", help="path to source dir")
     args = parser.parse_args()
-    
-    train = []
-    val = []
-    test = []
+
     idx = 0
-    
-    for speaker in tqdm(os.listdir(args.source_dir)):
-        wavs = os.listdir(os.path.join(args.source_dir, speaker))
-        shuffle(wavs)
-        train += wavs[2:-10]
-        val += wavs[:2]
-        test += wavs[-10:]
-        
-    shuffle(train)
-    shuffle(val)
-    shuffle(test)
-            
-    print("Writing", args.train_list)
+
+    args.source_dir: Path = args.source_dir
+
+    e = next(args.source_dir.iterdir())
+    f = list(map(lambda x: x.name[4:], e.glob("*.wav")))
+    shuffle(f)
+    le = len(f)
+    tr_le = int(0.9*le)
+    te_le = int(0.95*le)
+    train = f[:tr_le]
+    test = f[tr_le:te_le]
+    val = f[te_le:]
+
+    train_e = []
+    for sample in train:
+        train_e += list(args.source_dir.rglob(f"*{sample}"))
+
     with open(args.train_list, "w") as f:
-        for fname in tqdm(train):
-            speaker = fname[:4]
-            wavpath = os.path.join("DUMMY", speaker, fname)
-            f.write(wavpath + "\n")
-        
-    print("Writing", args.val_list)
-    with open(args.val_list, "w") as f:
-        for fname in tqdm(val):
-            speaker = fname[:4]
-            wavpath = os.path.join("DUMMY", speaker, fname)
-            f.write(wavpath + "\n")
-            
-    print("Writing", args.test_list)
+        for fname in train_e:
+            f.write(str(fname) + "\n")
+    
+    test_e = []
+    for sample in test:
+        test_e += list(args.source_dir.rglob(f"*{sample}"))
+
     with open(args.test_list, "w") as f:
-        for fname in tqdm(test):
-            speaker = fname[:4]
-            wavpath = os.path.join("DUMMY", speaker, fname)
-            f.write(wavpath + "\n")
-            
+        for fname in test_e:
+            f.write(str(fname) + "\n")
+    
+    val_e = []
+    for sample in val:
+        val_e += list(args.source_dir.rglob(f"*{sample}"))
+
+    with open(args.val_list, "w") as f:
+        for fname in val_e:
+            f.write(str(fname) + "\n")
